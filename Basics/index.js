d824421615cba5007dddfbcbce646f2e1b62fd55
Subhan Akbar <subhanakbar346@gmail.com>
2026-06-13 01:18:37 +0500

    Learning javascript day1

---
+++ Basics/index.js
@@ -0,0 +1,13 @@
+console.log(`Hello`)
+console.log(`My name is Subhan`)
+
+//window.alert("Hello World!");
+//window.alert("My name is Subhan");
+
+document.getElementById('myH1').textContent = `Hello`
+document.getElementById('myP').textContent = `My name is Subhan`
+
+//This is comment
+/*
+THIS A COMMENT */
+
