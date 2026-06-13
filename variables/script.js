2c984712be669f48bac96dadf8ebde6e0e9b250e
Subhan <subhanakbar346@gmail.com>
2026-06-13 22:57:55 +0500

    Learning about variables

---
+++ variables/script.js
@@ -0,0 +1,13 @@
+// A variable is a container that we can store values in. It behaves like the value its contains
+
+// Declaration : let x;
+// Intinalization : x = 10;
+
+//NOTE : EVERY VARIABLES HAVE TO BE UNIQUE, ELSE IT CAN LEAD TO PROBLEMS.
+
+let x;
+x = 100;
+
+console.log(x
+    
+)
