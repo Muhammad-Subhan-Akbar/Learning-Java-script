c1d4d93a3de709d13f0e76ee56f8ec0436233c19
Subhan <subhanakbar346@gmail.com>
2026-06-14 23:16:40 +0500

    varables

--- variables/script.js
+++ variables/script.js
@@ -5,9 +5,10 @@
 
 //NOTE : EVERY VARIABLES HAVE TO BE UNIQUE, ELSE IT CAN LEAD TO PROBLEMS.
 
-let x;
-x = 100;
+//This is declaration and assigment seperatly
+// let x;
+//x = 100;
 
-console.log(x
-    
-)
+//This is declaration and assigment combined
+let x = 100
+console.log(x)
