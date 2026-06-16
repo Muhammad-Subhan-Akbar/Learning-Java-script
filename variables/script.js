6e972f67a6880563a1a27d0a300ca8eca13b5ce6
Subhan <subhanakbar346@gmail.com>
2026-06-16 23:18:33 +0500

    learned about variables in Javascript

--- variables/script.js
+++ variables/script.js
@@ -10,5 +10,25 @@
 //x = 100;
 
 //This is declaration and assigment combined
-let x = 100
-console.log(x)
+// let x = 100
+// console.log(x)
+//You can find the type of data from the command typeof in the console.log.
+//  For example console.log(Type of (age))
+
+let name = "Subhan";
+let age = 17;
+let price = 45.99;
+let gpa = 2.1;
+
+
+console.log(`Your name is: ${name}`)
+console.log(typeof(name))
+
+console.log(`You are ${age} years old`);
+console.log(typeof(age))
+
+console.log(`The price is: ${price}`)
+console.log(typeof(price))
+
+console.log(`Your GPA is: ${gpa}`)
+console.log(typeof(gpa))
